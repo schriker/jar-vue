1acf710964e4a1e0786c5740899f57d23682ae89
Marcin Janus <marcinjanus@MacBook-Pro-Marcin.local>
2018-12-10 19:50:51 +0100

    Header menu items ui added

---
+++ src/components/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header row">
+      <ul>
+        <li v-for="n in 2" :key="n">
+          <a href="#">
+            <app-stream></app-stream>
+          </a>
+        </li>
+      </ul>
+    </header>
+</template>
+
+<script>
+import AppStream from './Stream'
+
+export default {
+  components: {
+    AppStream
+  }
+}
+</script>
---
+++ src/components/header/Stream.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="stream">
+        <div class="stream__icon">
+            <div class="stream__badge">4</div>
+            <div class="stream__remove">
+                <i class="fas fa-minus-circle"></i>
+            </div>
+            <img src="../../assets/default_avatar.png" alt="Avatar">
+        </div>
+        <div class="stream__title">
+            <span>Wonziu</span>
+            <div class="stream__status">
+                <span>ON</span> 359
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>

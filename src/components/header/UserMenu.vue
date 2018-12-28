86e114b99f1ddf920a5ef7ec13c293dd19c3ab38
schriker <janekmachine@gmail.com>
2018-12-28 17:36:08 +0100

    Add icon changed

--- src/components/header/UserMenu.vue
+++ src/components/header/UserMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="user-menu user-menu--links">
-    <li><router-link to="/add" active-class="active"><img :src="addIcon" alt=""></router-link></li>
+    <li><router-link to="/add" active-class="active"><i class="far fa-plus-square"></i></router-link></li>
     <li><router-link to="/bookmarks" active-class="active"><i class="far fa-bookmark"></i></router-link></li>
     <li v-if="user.data"><a @click.prevent="logOut"><i class="far fa-times-circle"></i></a></li>
     <li v-if="!user.data"><a :class="{ active: user.showUserModal }" @click.prevent="toggleUserModal" active-class="active"><i class="far fa-user"></i></a></li>
@@ -8,14 +8,8 @@
 </template>
 <script>
 import { mapState, mapActions, mapMutations } from 'vuex'
-import addIcon from '../../assets/add_new.svg'
 
 export default {
-  data () {
-    return {
-      addIcon
-    }
-  },
   computed: {
     ...mapState([
       'user'
